641b92ad63d28ff9023a0d8c464994ec8b9a35af
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 13:33:15 -0300

    feat: adiciona dto referente a saida de dados de school

--- backend/app/school/application/dtos.py
+++ backend/app/school/application/dtos.py
@@ -13,3 +13,28 @@ class SchoolInDTO(BaseModel):
     time_send_lunch: time
     time_send_snack_afternoon: time
     number_whats: str
+
+
+class SchoolOutDTO(BaseModel):
+    id: UUID 
+    name: str 
+    time_closing_presence: time 
+    time_send_lunch: time 
+    time_send_snack_afternoon: time 
+    number_whats: str 
+    created_at: datetime 
+    deleted_at: datetime 
+
+    @classmethod
+    def from_domain(cls, model):
+        return cls(
+            id=model.id,
+            name=model.name,
+            time_closing_presence=model.time_closing_presence,
+            time_send_lunch=model.time_send_lunch,
+            time_send_snack_afternoon=model.time_send_snack_afternoon,
+            number_whats=model.number_whats,
+            created_at=model.created_at,
+            deleted_at=model.deleted_at
+        )
+
