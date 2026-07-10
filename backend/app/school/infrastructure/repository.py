96280971d2b52b882d0d6ab09fc6bd00e2828193
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 13:53:32 -0300

    feat: implementa repository referente a managers

--- backend/app/school/infrastructure/repository.py
+++ backend/app/school/infrastructure/repository.py
@@ -1,9 +1,9 @@
 from typing import List
 from uuid import UUID
 
-from app.school.domain.entites import SchoolEntity
-from app.school.domain.repositories import ISchoolRepository
-from app.school.infrastructure.models import School
+from app.school.domain.entites import ManagerEntity, SchoolEntity
+from app.school.domain.repositories import IManagerRepository, ISchoolRepository
+from app.school.infrastructure.models import Manager, School
 
 
 class DjangoSchoolRepository(ISchoolRepository):
@@ -58,3 +58,67 @@ class DjangoSchoolRepository(ISchoolRepository):
             created_at=model.created_at,
             deleted_at=model.deleted_at
         )
+
+
+class DjangoManagerRepository(IManagerRepository):
+    def save(self, entity: ManagerEntity) -> ManagerEntity:
+        Manager.objects.update_or_create(
+            id=id,
+            defaults={
+                'school_id': entity.school_id,
+                'role': entity.role,
+                'name': entity.name,
+                'email': entity.email,
+                'password': entity.password,
+                'active': entity.active,
+                'created_at': entity.created_at
+            }
+        )
+        
+        return entity
+    
+    def find_by_id(self, id: UUID) -> ManagerEntity | None:
+        try:
+            return self._to_model(Manager.objects.get(id=id))
+
+        except Manager.DoesNotExist:
+            return None
+        
+    def find_by_email(self, email: str) -> ManagerEntity | None:
+        try:
+            return self._to_model(Manager.objects.get(email=email))
+
+        except Manager.DoesNotExist:
+            return None
+    
+    def lists_managers_by_actives(self) -> List[ManagerEntity]:
+        try:
+            return [
+                self._to_model(entity)
+                for entity in Manager.objects.filter(active=True).all()
+            ]
+        
+        except Manager.DoesNotExist:
+            return []
+        
+    def lists_managers_role_by_role(self, role: str) -> List[ManagerEntity]:
+        try:
+            return [
+                self._to_model(entity)
+                for entity in Manager.objects.filter(role=role)
+            ]
+        
+        except Manager.DoesNotExist:
+            return []
+
+    def _to_model(self, model: Manager) -> ManagerEntity:
+        return ManagerEntity(
+            id=model.id,
+            school_id=model.school_id,
+            role=model.role,
+            email=model.email,
+            password=model.password,
+            active=model.active,
+            created_at=model.created_at
+        )
+    
