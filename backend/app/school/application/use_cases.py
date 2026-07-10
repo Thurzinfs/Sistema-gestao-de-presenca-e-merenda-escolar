25f066f2fa28051ac3561b9dd58a274676e52655
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 14:08:05 -0300

    feat: adiciona caso de uso referente ao registro de school

---
+++ backend/app/school/application/use_cases.py
@@ -0,0 +1,24 @@
+from app.school.application.dtos import SchoolInDTO, SchoolOutDTO
+from app.school.domain.entites import SchoolEntity
+from app.school.domain.exceptions import SchoolNotFoundException
+from app.school.domain.repositories import ISchoolRepository
+
+
+class RegisterSchoolUseCase:
+    def __init__(self, school_repo: ISchoolRepository) -> None:
+        self.school_repo = school_repo
+
+    def execute(self, dto: SchoolInDTO) -> SchoolOutDTO:
+        if not self.school_repo.verify_exists_school_by_name(dto.name):
+            raise SchoolNotFoundException('school not found')
+        
+        school = SchoolEntity(
+            name=dto.name,
+            time_closing_presence=dto.time_closing_presence,
+            time_send_lunch=dto.time_send_lunch,
+            time_send_snack_afternoon=dto.time_send_snack_afternoon,
+            number_whats=dto.number_whats
+        )
+
+        self.school_repo.save(school)
+        return SchoolOutDTO.from_domain(school)
