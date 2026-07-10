8b42edd476b9455c69e42ca95383020584c236d4
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 14:24:56 -0300

    feat: adiciona caso de uso para atualizar school

--- backend/app/school/application/use_cases.py
+++ backend/app/school/application/use_cases.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from app.school.application.dtos import SchoolInDTO, SchoolOutDTO
+from app.school.application.dtos import SchoolInDTO, SchoolInUpdateDTO, SchoolOutDTO
 from app.school.domain.entites import SchoolEntity
 from app.school.domain.exceptions import SchoolNotActiveException, SchoolNotFoundException
 from app.school.domain.repositories import ISchoolRepository
@@ -38,5 +38,36 @@ class ResponseSchoolUseCase:
         if school.deleted_at is not None:
             raise SchoolNotActiveException('school not active')
         
+        return SchoolOutDTO.from_domain(school)
+    
+
+class UpdateSchoolUseCase:
+    def __init__(self, school_repo: ISchoolRepository) -> None:
+        self.school_repo = school_repo
+
+    def execute(self, id: UUID, dto: SchoolInUpdateDTO) -> SchoolOutDTO:
+        school = self.school_repo.find_by_id(id)
+        if not school:
+            raise SchoolNotFoundException('school not found')
+        
+        if school.deleted_at is not None:
+            raise SchoolNotActiveException('school not active')
+        
+        if dto.name:
+            school.change_name(dto.name)
+
+        if dto.time_closing_presence:
+            school.change_time_closing_presence(dto.time_closing_presence)
+
+        if dto.time_send_lunch:
+            school.change_time_send_lunch(dto.time_send_lunch)
+
+        if dto.time_send_snack_afternoon:
+            school.change_time_snack_afternoon(dto.time_send_snack_afternoon)
+
+        if dto.number_whats:
+            school.change_number_whats(dto.number_whats)
+
+        self.school_repo.save(school)
         return SchoolOutDTO.from_domain(school)
     
