bb2ccf95b3729264e2f0feffd6d9489c4f79170e
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 16:24:32 -0300

    feat: implementa as injecoes de dependencia referentes ao manager

--- backend/app/school/api/dependencies.py
+++ backend/app/school/api/dependencies.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
 
-from app.school.application.use_cases import DeactiveSchoolUseCase, RegisterSchoolUseCase, ResponseSchoolUseCase, UpdateSchoolUseCase
-from app.school.infrastructure.repository import DjangoSchoolRepository
+from app.school.application.use_cases import DeactiveManagerUseCase, DeactiveSchoolUseCase, RegisterManagerUseCase, RegisterSchoolUseCase, ResponseManagerByIDUseCase, ResponseSchoolUseCase, UpdateManagerUseCase, UpdateSchoolUseCase
+from app.school.infrastructure.repository import DjangoManagerRepository, DjangoSchoolRepository
 
 
 class SchoolContainer(containers.DeclarativeContainer):
@@ -26,3 +26,26 @@ class SchoolContainer(containers.DeclarativeContainer):
         DeactiveSchoolUseCase,
         school_repo=school_repo
     )
+
+    manager_repo = providers.Factory(DjangoManagerRepository)
+
+    register_manager_use_case = providers.Factory(
+        RegisterManagerUseCase,
+        manager_repo=manager_repo
+    )
+
+    response_manager_use_case = providers.Factory(
+        ResponseManagerByIDUseCase,
+        manager_repo=manager_repo
+    )
+
+    update_manager_use_case = providers.Factory(
+        UpdateManagerUseCase,
+        manager_repo=manager_repo
+    )
+
+    deactive_manager_use_case = providers.Factory(
+        DeactiveManagerUseCase,
+        manager_repo=manager_repo
+    )
+    
