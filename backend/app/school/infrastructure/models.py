70a1bb58f9b91fee64cb6557850a6b6def63af7f
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-09 21:48:13 -0300

    feat: adiciona o model referente a school

--- backend/app/school/infrastructure/models.py
+++ backend/app/school/infrastructure/models.py
@@ -1,2 +1,18 @@
+from uuid import uuid4
+
 from django.db import models
+
+
+class School(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    name = models.CharField(max_length=180)
+    time_closing_presence = models.TimeField()
+    time_send_lunch = models.TimeField()
+    time_send_snack_afternoon = models.TimeField()
+    number_whats = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, default=None)
+
+    class Meta:
+        db_table = 'schools'
 
