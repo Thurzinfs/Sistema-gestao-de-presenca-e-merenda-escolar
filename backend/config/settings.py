a6597abf2c821c5bc0691084d6c50e21269208f3
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 22:22:48 -0300

    feat: implementa o uso do banco postgres no backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'PORT': os.getenv('DB_PORT'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'USER': os.getenv('DB_USER'),
+        'HOST': 'database',
     }
 }
+
 
 
 # Password validation
