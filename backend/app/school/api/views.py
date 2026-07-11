b3c64b2f4e1aa776c387b7c110c14d5b9a661daf
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 22:19:19 -0300

    feat: implementa a rota referente a criacao de school

---
+++ backend/app/school/api/views.py
@@ -0,0 +1,26 @@
+from uuid import UUID
+
+from ninja import Router
+
+from app.school.api.dependencies import SchoolContainer
+from app.school.api.schemas import ManagerIn, ManagerOut, ManagerUpdate, SchoolIn, SchoolOut, SchoolUpdate
+
+from django.db.transaction import atomic
+
+router_school = Router()
+
+router_manager = Router()
+
+container = SchoolContainer()
+
+
+@router_school.post('/', response={201: SchoolOut})
+@atomic
+def register_school(request, data: SchoolIn):
+    dto = data.to_dto()
+
+    use_case = container.register_school_use_case()
+
+    school = use_case.execute(dto)
+
+    return SchoolOut.from_domain(school)
