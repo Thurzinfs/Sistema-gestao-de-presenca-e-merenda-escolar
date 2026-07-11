8a3a3721796b44517a0265d142ae6734198cb78f
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-10 22:09:06 -0300

    feat: adiciona value object referente ao time registrado em school

---
+++ backend/app/school/domain/value_objects.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import time
+
+
+@dataclass(frozen=True)
+class SchoolTimeVO:
+    value: time
+
+    def __post_init__(self):
+        object.__setattr__(self, 'value', self.value.replace(tzinfo=None))
+    
+    def __str__(self) -> str:
+        return self.value.strftime('%H:%M:%S')
