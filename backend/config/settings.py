09f56f3bc9457a9282dc61563fe46002ac30a580
silvaphb <jeferson.aps170@gmail.com>
2026-07-17 09:27:19 -0300

    chore: Adição de cantina na aplicação

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.school'
+    'app.school',
+    'app.canteen'
 ]
 
 MIDDLEWARE = [
