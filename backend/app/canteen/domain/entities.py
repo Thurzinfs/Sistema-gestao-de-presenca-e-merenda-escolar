945cc81c7616b50cb437866b553e1f2e532ad457
silvaphb <jeferson.aps170@gmail.com>
2026-07-17 09:58:18 -0300

    fix: Correção de estrutura do DailyMenuEntity

--- backend/app/canteen/domain/entities.py
+++ backend/app/canteen/domain/entities.py
@@ -1,12 +1,20 @@
-from datetime import datetime, date
-from dataclasses import dataclass
-from uuid import UUID
+from datetime import datetime, date as Date
+from dataclasses import dataclass, field
+from uuid import UUID, uuid4
 
 @dataclass
 class DailyMenuEntity:
-    id: UUID
-    school: UUID
-    date: date
-    main_course: str
-    manager: UUID
-    created_at: datetime
+    id: UUID = field(default_factory=uuid4)
+    school: UUID | None = field(default=None)
+    date: Date | None = field(default=None)
+    main_course: str = field(default='')
+    manager: UUID | None = field(default=None)
+    created_at: datetime = field(default_factory=datetime.now)
+
+    def change_date(self, date):
+        if date:
+            self.date = date
+    
+    def change_main_course(self, main_course):
+        if main_course:
+            self.main_course = main_course
