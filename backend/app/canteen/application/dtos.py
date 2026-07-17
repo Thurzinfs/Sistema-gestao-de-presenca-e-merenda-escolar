a6f73f4c7e66ed36de3f70c54f8cc5023afb1c69
silvaphb <jeferson.aps170@gmail.com>
2026-07-17 10:13:06 -0300

    feat: Criação dos DTOs da aplicação canteen

---
+++ backend/app/canteen/application/dtos.py
@@ -0,0 +1,31 @@
+from uuid import UUID
+from typing import Optional
+from datetime import datetime, date as Date
+
+class DailyMenuInDTO():
+    school: UUID
+    date: Date
+    main_course: str
+    manager: UUID
+    created_at: datetime
+
+class DailyMenuOutDTO():
+    id: UUID
+    date: Date
+    main_course: str
+    manager: UUID
+    created_at: datetime
+
+    @classmethod
+    def from_domain(cls, entity):
+        return cls(
+            id=entity.id,
+            date=entity.date,
+            main_course=entity.main_course,
+            manager=entity.manager,
+            created_at=entity.created_at
+        )
+    
+class DailyMenuUpdateDTO():
+    date: Optional[UUID] = None
+    main_course: Optional[str] = None
