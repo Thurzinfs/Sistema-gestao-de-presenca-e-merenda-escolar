4c5d379c1b8667d64b7f356f992b085c007ba9ff
silvaphb <jeferson.aps170@gmail.com>
2026-07-18 10:14:26 -0300

    feat: Criação do metodo POST (Criação de cardapio) referente a aplicação canteen

--- backend/app/canteen/api/views.py
+++ backend/app/canteen/api/views.py
@@ -1,8 +1,15 @@
 from ninja import Router
+from django.db.transaction import atomic
+from app.canteen.api.dependencies import DailyMenuContainer
+from app.canteen.api.schemas import DailyMenuIn, DailyMenuOut
 
 router = Router()
+container = DailyMenuContainer
 
-
-@router.post('')
-def register(request):
-    ...
+@router.post('/', response={201: DailyMenuOut})
+@atomic
+def register_daily_menu(request, data: DailyMenuIn):
+    use_case = container.register_daily_menu_use_case()
+    dto = data.to_dto()
+    response = use_case.execute(dto)
+    return 201, DailyMenuOut.from_domain(response)
