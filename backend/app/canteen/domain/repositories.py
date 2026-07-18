7177978f3f89dd5eb510c91eebf01e9f0b2bebb1
silvaphb <jeferson.aps170@gmail.com>
2026-07-18 08:15:36 -0300

    feat: Criação do classe responsavel pela abstração da aplicação canteen

---
+++ backend/app/canteen/domain/repositories.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from datetime import date
+from uuid import UUID
+from app.canteen.domain.entities import DailyMenuEntity
+
+class IDailyMenuRepository(ABC):
+    @abstractmethod
+    def save(self, daily_menu: DailyMenuEntity) -> DailyMenuEntity:
+        ...
+
+    @abstractmethod
+    def find_by_id(self, id: UUID) -> DailyMenuEntity | None:
+        ...
+    
+    @abstractmethod
+    def find_by_date(self, date: date) -> DailyMenuEntity | None:
+        ...
+
+    @abstractmethod
+    def verify_exists(self, id: UUID) -> bool:
+        ...
+    
+    @abstractmethod
+    def verify_by_date(self, date: date) -> bool:
+        ...
+    
