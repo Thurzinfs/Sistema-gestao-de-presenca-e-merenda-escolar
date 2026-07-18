364118613731bcf956028d16eb5fc94d8ca9e135
silvaphb <jeferson.aps170@gmail.com>
2026-07-18 10:52:16 -0300

    fix: Correção de verificação de retorno de entidades referente ao model DailyMenu

--- backend/app/canteen/application/use_cases.py
+++ backend/app/canteen/application/use_cases.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 from app.canteen.application.dtos import DailyMenuInDTO, DailyMenuOutDTO, DailyMenuUpdateDTO
 from app.canteen.domain.entities import DailyMenuEntity
+from app.canteen.domain.exceptions import NotFoundCanteenException
 from app.canteen.domain.repositories import IDailyMenuRepository
 from datetime import date as Date
 
@@ -9,8 +10,8 @@ class RegisterDailyMenuUseCase:
         self.daily_menu_repo = daily_menu_repo
     
     def execute(self, dto: DailyMenuInDTO) -> DailyMenuOutDTO:
-        if self.daily_menu_repo.find_by_date(dto.date):
-            raise ...
+        if not self.daily_menu_repo.verify_by_date(dto.date):
+            raise NotFoundCanteenException('not found daily menu by date')
         
         daily_Menu = DailyMenuEntity(
             school=dto.school,
@@ -30,7 +31,7 @@ class ReturnDailyMenuUseCase:
         entity = self.daily_menu_repo.find_by_date(date)
 
         if not entity:
-            raise ...
+            raise NotFoundCanteenException('not found daily menu by date')
         
         return DailyMenuOutDTO.from_domain(entity)
 
@@ -41,7 +42,7 @@ class UpdateDailyMenuUseCase:
     def execute(self, id: UUID, dto: DailyMenuUpdateDTO) -> DailyMenuOutDTO:
         entity = self.daily_menu_repo.find_by_id(id)
         if not entity:
-            raise ...
+            raise NotFoundCanteenException('not found daily menu by date')
         
         if dto.date:
             entity.change_date(dto.date)
