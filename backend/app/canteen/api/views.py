d8a7c1ddbd22c5e5dbb21a96f8772b3a8874a0a4
silvaphb <jeferson.aps170@gmail.com>
2026-07-18 11:55:31 -0300

    feat: Criação do metodo PATCH (Atualização de DailyMenu) da aplicação canteen

--- backend/app/canteen/api/views.py
+++ backend/app/canteen/api/views.py
@@ -1,10 +1,9 @@
+from uuid import UUID
 from ninja import Router
 from django.db.transaction import atomic
 from app.canteen.api.dependencies import DailyMenuContainer
-from app.canteen.api.schemas import DailyMenuIn, DailyMenuOut
+from app.canteen.api.schemas import DailyMenuIn, DailyMenuOut, DailyMenuUpdate
 from datetime import date as Date
-
-from app.canteen.infrastructure.models import DailyMenu
 
 router = Router()
 container = DailyMenuContainer
@@ -21,4 +20,12 @@ def register_daily_menu(request, data: DailyMenuIn):
 def view_daily_menu(request, date: Date):
     use_case = container.return_daily_menu_use_case()
     response = use_case.execute(date)
+    return 200, DailyMenuOut.from_domain(response)
+
+@router.patch('/{id}', response={200: DailyMenuOut})
+@atomic
+def update_daily_menu(request, id: UUID, data: DailyMenuUpdate):
+    use_case = container.update_daily_menu_use_case()
+    dto = data.to_dto()
+    response = use_case.execute(id, dto)
     return 200, DailyMenuOut.from_domain(response)
