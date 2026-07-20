43bf3f38d20a13c6f08afc85516c018007ac3e6d
silvaphb <jeferson.aps170@gmail.com>
2026-07-20 08:30:09 -0300

    feat: Criação de nova injeção de dependencia (ReturnWithDateRangeUseCase) na aplicação canteen

--- backend/app/canteen/api/dependencies.py
+++ backend/app/canteen/api/dependencies.py
@@ -1,9 +1,11 @@
 from dependency_injector import providers, containers
-from app.canteen.application.use_cases import RegisterDailyMenuUseCase, ReturnDailyMenuUseCase, UpdateDailyMenuUseCase
+from app.canteen.application.use_cases import RegisterDailyMenuUseCase, ReturnDailyMenuUseCase, ReturnWithDateRangeUseCase, UpdateDailyMenuUseCase
 from app.canteen.infrastructure.repository import DailyMenuRepository
+from app.canteen.infrastructure.service import PickDatesService
 
 class DailyMenuContainer(containers.DeclarativeContainer):
     daily_menu_repo = providers.Factory(DailyMenuRepository)
+    pick_dates_service = providers.Factory(PickDatesService)
 
     register_daily_menu_use_case = providers.Factory(
         RegisterDailyMenuUseCase,
@@ -18,4 +20,9 @@ class DailyMenuContainer(containers.DeclarativeContainer):
     update_daily_menu_use_case = providers.Factory(
         UpdateDailyMenuUseCase,
         daily_menu_repo = daily_menu_repo
+    )
+
+    return_with_date_range_use_case = providers.Factory(
+        ReturnWithDateRangeUseCase,
+        pick_dates_service = pick_dates_service
     )
