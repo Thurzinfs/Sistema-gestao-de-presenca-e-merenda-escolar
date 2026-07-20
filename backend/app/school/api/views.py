248a2ed8f93240607663d3076fa4db523da4221c
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-20 11:01:32 -0300

    feat: adiciona as rotas referentes a autenticacao de usuario

--- backend/app/school/api/views.py
+++ backend/app/school/api/views.py
@@ -1,15 +1,19 @@
+from typing import List
 from uuid import UUID
 
 from ninja import Router
 
+from app.school.api.bearer import AuthBearer
 from app.school.api.dependencies import SchoolContainer
-from app.school.api.schemas import ManagerIn, ManagerOut, ManagerUpdate, SchoolIn, SchoolOut, SchoolUpdate
+from app.school.api.schemas import LoginIn, LoginOut, ManagerIn, ManagerOut, ManagerUpdate, SchoolIn, SchoolOut, SchoolUpdate
 
 from django.db.transaction import atomic
 
 router_school = Router()
 
 router_manager = Router()
+
+router_auth = Router()
 
 container = SchoolContainer()
 
@@ -24,6 +28,17 @@ def register_school(request, data: SchoolIn):
     school = use_case.execute(dto)
 
     return SchoolOut.from_domain(school)
+
+@router_school.get('/list/actives', response={200: List[SchoolOut]})
+def list_schools(request):
+    use_case = container.list_schools_use_case()
+
+    schools = use_case.execute()
+
+    return 200, [
+        SchoolOut.from_domain(school)
+        for school in schools
+    ]
 
 @router_school.get('/{id}', response={200: SchoolOut})
 def response_school(request, id: UUID):
@@ -91,3 +106,17 @@ def deactive_manager(request, id: UUID):
     manager = use_case.execute(id)
 
     return ManagerOut.from_domain(manager)
+
+@router_auth.post('/', response={201: LoginOut})
+def login(request, data: LoginIn):
+    dto = data.to_dto()
+
+    use_case = container.login_use_case()
+
+    login = use_case.execute(dto)
+
+    return 201, LoginOut.from_domain(login)
+
+@router_auth.get('/me', response={200: ManagerOut}, auth=AuthBearer())
+def request_me(request):
+    return ManagerOut.from_domain(request.auth)
