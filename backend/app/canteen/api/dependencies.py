23e778e79387d477e0f652fbf1e952df1dca3532
silvaphb <jeferson.aps170@gmail.com>
2026-07-23 18:35:28 -0300

    fix: correção de importações e parametros passados nos casos de uso referente ao model LeftouversLunch da aplicação canteen

--- backend/app/canteen/api/dependencies.py
+++ backend/app/canteen/api/dependencies.py
@@ -7,11 +7,12 @@ from app.canteen.application.use_cases import (
     UpdateDailyMenuUseCase,
     RegisterLeftouversLunchUseCase,
     ReturnLeftouversLunchWithIdUseCase,
-    ReturnLeftouversLunchWithDateUseCase,
+    ReturnLeftouversLunchWithMonthUseCase,
     UpdateLeftouversLunchUseCase
 )
 from app.canteen.infrastructure.repository import DailyMenuRepository, LeftouversLunchRepository
 from app.canteen.infrastructure.service import PickDatesService, VerifyLeftouverLunchExistsService
+from app.school.infrastructure.repository import DjangoManagerRepository
 
 
 class DailyMenuContainer(containers.DeclarativeContainer):
@@ -41,15 +42,16 @@ class DailyMenuContainer(containers.DeclarativeContainer):
 class LeftouversLunchContainer(containers.DeclarativeContainer):
     leftouvers_lunch_repo = providers.Factory(LeftouversLunchRepository)
     leftouvers_lunch_exists_service = providers.Factory(VerifyLeftouverLunchExistsService)
+    manager_repo = providers.Factory(DjangoManagerRepository)
 
     register_leftouvers_lunch_use_case = providers.Factory(
-        RegisterLeftouversLunchUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo, leftouvers_lunch_exists_service=leftouvers_lunch_exists_service
+        RegisterLeftouversLunchUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo, leftouvers_lunch_exists_service=leftouvers_lunch_exists_service, manager_repo = manager_repo
     )
     return_with_id_leftouvers_lunch_use_case = providers.Factory(
         ReturnLeftouversLunchWithIdUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo
     )
-    return_with_date_leftouvers_lunch_use_case = providers.Factory(
-        ReturnLeftouversLunchWithDateUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo
+    return_with_month_leftouvers_lunch_use_case = providers.Factory(
+        ReturnLeftouversLunchWithMonthUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo
     )
     update_leftouvers_lunch_use_case = providers.Factory(
         UpdateLeftouversLunchUseCase, leftouvers_lunch_repo=leftouvers_lunch_repo
