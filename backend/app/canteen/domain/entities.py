17980606ef1cb787fe73d5ae811862a598c385e9
silvaphb <jeferson.aps170@gmail.com>
2026-07-23 08:32:14 -0300

    feat: criação da entidade referente ao model LeftouversLunch

--- backend/app/canteen/domain/entities.py
+++ backend/app/canteen/domain/entities.py
@@ -19,3 +19,19 @@ class DailyMenuEntity:
     def change_main_course(self, main_course):
         if main_course:
             self.main_course = main_course
+
+@dataclass
+class LeftouversLunchEntity:
+    id: UUID = field(default_factory=uuid4)
+    school: UUID | None = field(default=None)
+    leftouvers_kg: int = field(default=0)
+    amount_students: int = field(default=0)
+    created_at: datetime = field(default_factory=datetime.now)
+
+    def change_leftouvers_kg(self, leftouvers_kg):
+        if leftouvers_kg:
+            self.leftouvers_kg = leftouvers_kg
+
+    def change_amount_students(self, amount_students):
+        if amount_students:
+            self.amount_students = amount_students
