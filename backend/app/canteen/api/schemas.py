b05be5f26dfb61aeb026fa1d573f12e517ed2ee9
silvaphb <jeferson.aps170@gmail.com>
2026-07-23 08:57:56 -0300

    feat: criação dos schemas referentes ao model LeftouversLunch

--- backend/app/canteen/api/schemas.py
+++ backend/app/canteen/api/schemas.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from datetime import datetime, date as Date
 from ninja import Schema
 
-from app.canteen.application.dtos import DailyMenuInDTO, DailyMenuUpdateDTO
+from app.canteen.application.dtos import DailyMenuInDTO, DailyMenuUpdateDTO, LeftouversLunchInDTO, LeftouversLunchUpdateDTO
 
 
 class DailyMenuIn(Schema):
@@ -47,3 +47,46 @@ class DailyMenuUpdate(Schema):
 
     def to_dto(self) -> DailyMenuUpdateDTO:
         return DailyMenuUpdateDTO(date=self.date, main_course=self.main_course)
+
+class LeftouversLunchIn(Schema):
+    school: UUID
+    leftouvers_kg: int
+    amount_students: int
+    user: UUID
+
+    def to_dto(self) -> LeftouversLunchInDTO:
+        return LeftouversLunchInDTO(
+            school=self.school,
+            leftouvers_kg=self.leftouvers_kg,
+            amount_students=self.amount_students,
+            user=self.user
+        )
+
+class LeftouversLunchOut(Schema):
+    id: UUID
+    school: UUID
+    leftouvers_kg: int
+    amount_students: int
+    user: UUID
+    created_at: datetime
+
+    @staticmethod
+    def from_domain(entity):
+        return LeftouversLunchOut(
+            id=entity.id,
+            school=entity.school,
+            leftouvers_kg=entity.leftouvers_kg,
+            amount_students=entity.amount_students,
+            user=entity.user,
+            created_at=entity.created_at
+        )
+
+class LeftouversLunchUpdate(Schema):
+    leftouvers_kg: Optional[int] = None
+    amount_students: Optional[int] = None
+
+    def to_dto(self) -> LeftouversLunchUpdateDTO:
+        return LeftouversLunchUpdateDTO(
+            leftouvers_kg=self.leftouvers_kg,
+            amount_students=self.amount_students
+        )
