5bcae5ba2e702d24ead0e4578c57bfb628ea6b27
silvaphb <jeferson.aps170@gmail.com>
2026-07-23 12:46:16 -0300

    feat: criação do metodo GET de retornar relatorio por determinado ID referente ao model LeftouversLunch da aplicação canteen

--- backend/app/canteen/api/views.py
+++ backend/app/canteen/api/views.py
@@ -61,3 +61,9 @@ def view_by_date(request, date: Date):
     use_case = leftouverslunch_container.return_with_date_leftouvers_lunch_use_case()
     response = use_case.execute(date)
     return 200, LeftouversLunchOut.from_domain(response)
+
+@leftouverslunch_router.get('/{id}', response={200: LeftouversLunchOut})
+def view_by_id(request, id: UUID):
+    use_case = leftouverslunch_container.return_with_id_leftouvers_lunch_use_case()
+    response = use_case.execute(id)
+    return 200, LeftouversLunchOut.from_domain(response)
