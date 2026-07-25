2102b4ad5ccbe042e1e490c4702115eb08640dfa
silvaphb <jeferson.aps170@gmail.com>
2026-07-25 09:45:51 -0300

    fix: corrige nomeclatura dos containers e ordem dos metodos

--- backend/app/canteen/api/views.py
+++ backend/app/canteen/api/views.py
@@ -9,34 +9,34 @@ from datetime import date as Date
 router = Router()
 leftouverslunch_router = Router()
 
-container = DailyMenuContainer
-leftouverslunch_container = LeftouversLunchContainer
+daily_menu_container = DailyMenuContainer()
+leftouverslunch_container = LeftouversLunchContainer()
 
 
 @router.post('/', response={201: DailyMenuOut})
 @atomic
 def register_daily_menu(request, data: DailyMenuIn):
-    use_case = container.register_daily_menu_use_case()
+    use_case = daily_menu_container.register_daily_menu_use_case()
     dto = data.to_dto()
     response = use_case.execute(dto)
     return 201, DailyMenuOut.from_domain(response)
 
 @router.get('/{id}', response={200: DailyMenuOut})
 def view_by_id(request, id: UUID):
-    use_case = container.return_with_id_use_case()
+    use_case = daily_menu_container.return_with_id_use_case()
     response = use_case.execute(id)
     return 200, DailyMenuOut.from_domain(response)
 
 @router.get('/', response={200: DailyMenuOut})
 def view_daily_menu(request, date: Date):
-    use_case = container.return_daily_menu_use_case()
+    use_case = daily_menu_container.return_daily_menu_use_case()
     response = use_case.execute(date)
     return 200, DailyMenuOut.from_domain(response)
 
 
 @router.get('/date_range', response={200: List[DailyMenuOut]})
 def view_with_date_range(request, from_date: Date, to_date: Date):
-    use_case = container.return_with_date_range_use_case()
+    use_case = daily_menu_container.return_with_date_range_use_case()
     entities = use_case.execute(from_date, to_date)
     return 200, [DailyMenuOut.from_domain(entity) for entity in entities]
 
@@ -44,7 +44,7 @@ def view_with_date_range(request, from_date: Date, to_date: Date):
 @router.patch('/{id}', response={200: DailyMenuOut})
 @atomic
 def update_daily_menu(request, id: UUID, data: DailyMenuUpdate):
-    use_case = container.update_daily_menu_use_case()
+    use_case = daily_menu_container.update_daily_menu_use_case()
     dto = data.to_dto()
     response = use_case.execute(id, dto)
     return 200, DailyMenuOut.from_domain(response)
@@ -57,17 +57,18 @@ def register_leftouvers_lunch(request, data: LeftouversLunchIn):
     response = use_case.execute(dto)
     return 201, LeftouversLunchOut.from_domain(response)
 
-@leftouverslunch_router.get('/{month}', response={200: LeftouversLunchOut})
-def view_leftouvers_by_month(request, month: int):
-    use_case = leftouverslunch_container.return_with_month_leftouvers_lunch_use_case()
-    response = use_case.execute(month)
-    return 200, LeftouversLunchOut.from_domain(response)
-
 @leftouverslunch_router.get('/{id}', response={200: LeftouversLunchOut})
 def view_leftouvers_by_id(request, id: UUID):
     use_case = leftouverslunch_container.return_with_id_leftouvers_lunch_use_case()
     response = use_case.execute(id)
     return 200, LeftouversLunchOut.from_domain(response)
+
+@leftouverslunch_router.get('/month/{month}', response={200: LeftouversLunchOut})
+def view_leftouvers_by_month(request, month: int):
+    use_case = leftouverslunch_container.return_with_month_leftouvers_lunch_use_case()
+    response = use_case.execute(month)
+    return 200, LeftouversLunchOut.from_domain(response)
+
 
 @leftouverslunch_router.patch('/{id}', response={200: LeftouversLunchOut})
 @atomic
