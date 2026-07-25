5b0671edc87d0e5e7ca98b43aaac2f24401b40f7
silvaphb <jeferson.aps170@gmail.com>
2026-07-25 09:39:43 -0300

    feat: adição de novo Exception para tratamento de erros de mes invalido no metodo PATCH referente ao model LeftouversLunch

--- backend/app/canteen/domain/exceptions.py
+++ backend/app/canteen/domain/exceptions.py
@@ -15,3 +15,6 @@ class AlreadyExistsCanteenException(BaseDomainException):
 
 class InvalidDateFieldCanteenException(BaseDomainException):
     pass
+
+class InvalidMonthCanteenException(BaseDomainException):
+    ...
