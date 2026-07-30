5a3e3032d3aacd2413156b2bbbf2fb7818878678
Francisco Matheus <franmatheussous@gmail.com>
2026-07-30 15:21:29 -0300

    feat: cria excecoes de negocio do dominio

---
+++ backend/app/academic/domain/exceptions.py
@@ -0,0 +1,20 @@
+from core.exceptions import BaseDomainException
+
+
+class ConflictFieldException(BaseDomainException):
+    pass
+
+class ClassroomAlreadyExistsException(BaseDomainException):
+    pass
+
+class ClassroomNotFoundException(BaseDomainException):
+    pass
+
+class ClassroomNotActiveException(BaseDomainException):
+    pass
+
+class StudentAlreadyExistsException(BaseDomainException):
+    pass
+
+class StudentsNotFoundException(BaseDomainException):
+    pass
