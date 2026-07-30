b46e8ecaef020b6e5ee6465c343fd5565e205866
Francisco Matheus <franmatheussous@gmail.com>
2026-07-30 15:17:58 -0300

    feat: adiciona DTOs de entrada e saida

---
+++ backend/app/academic/application/dtos.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from pydantic import BaseModel
+
+
+# Inp Classroom
+class ClassroomInDTO(BaseModel):
+    school: UUID
+    name: str
+
+# Outp Classroom
+class ClassroomOutDTO(BaseModel):
+    id: UUID
+    school: UUID
+    name: str
+    active: bool
+    created_at: datetime
+
+    @classmethod
+    def from_domain(cls, model):
+        return cls(
+            id=model.id,
+            school=model.school,
+            name=model.name,
+            active=model.active,
+            created_at=model.created_at
+        )
+
+# Update Classroom
+class ClassroomUpdateDTO(BaseModel):
+    name: Optional[str] = None
+
+# Inp Students
+class StudentsInDTO(BaseModel):
+    classroom: UUID
+    name: str
+    ra: str
+    qr_code: str
+
+# Outp Students
+class StudentsOutDTO(BaseModel):
+    id: UUID
+    classroom: UUID
+    name: str
+    ra: str
+    qr_code: str
+    active: bool
+    created_at: datetime
+
+    @classmethod
+    def from_domain(cls, model):
+        return cls(
+            id=model.id,
+            classroom=model.classroom,
+            name=model.name,
+            ra=model.ra,
+            qr_code=model.qr_code,
+            active=model.active,
+            created_at=model.created_at
+        )
+
+# Update Students
+class StudentsUpdateDTO(BaseModel):
+    name: Optional[str] = None
+    ra: Optional[str] = None
+    qr_code: Optional[str] = None    
