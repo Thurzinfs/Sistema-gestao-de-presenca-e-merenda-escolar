57e937b416f8b8b454f2b5a4dabc47bc85698b49
Pedro Henrique <pedrohenriquealencarpereira5@gmail.com>
2026-08-01 09:24:49 -0300

    feat: adiciona as tabelas referentes as entidades de presença

--- backend/app/presence/infrastructure/models.py
+++ backend/app/presence/infrastructure/models.py
@@ -1,13 +1,15 @@
+from black import mode
 from django.db import models
 from uuid import uuid4
 
-from  app.presence.domain.role import MomentRole
+from  app.presence.domain.role import MomentRole, SnackRole
 
 class Readings(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     student_id = models.ForeignKey(
-
+        'academic.Student', on_delete=models.CASCADE
     )
+
     moment = models.CharField(
         max_length=20, choices=MomentRole, default=MomentRole.snack_morning
     )
@@ -17,3 +19,37 @@ class Readings(models.Model):
     class Meta:
         db_table = 'readings'
 
+class Frequency(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False,default=uuid4)
+    student_id = models.ForeignKey(
+        'academic.Student', on_delete=models.CASCADE
+    )
+    date = models.DateField()
+    on_time = models.BooleanField(default=False)
+    reading_id = models.ForeignKey(
+        'presence.Readings', on_delete=models.CASCADE
+    )
+    class Meta:
+            db_table = 'frequencys'
+
+class RegisterSnack(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    student_id = student_id = models.ForeignKey(
+        'academic.Student', on_delete=models.CASCADE
+    )
+    date = models.DateField()
+
+    moment = models.CharField(
+        max_length=20, choices=MomentRole, default=MomentRole.snack_morning
+    )
+
+    type_snack = models.CharField(
+        max_length=200, choices=SnackRole, default=SnackRole.normal
+    )
+
+    reading_id = models.ForeignKey(
+        'presence.Readings', on_delete=models.CASCADE
+    )
+
+    class Meta:
+            db_table = 'register_snacks'
