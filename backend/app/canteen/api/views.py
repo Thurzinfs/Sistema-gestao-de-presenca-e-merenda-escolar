df65514e9aa87f0c1f1cc5f69a4478d66da18be0
Pedro Henrique <pedrohenriquealencarpereira5@gmail.com>
2026-08-15 10:01:02 -0300

    fix: corrige nomeclatura e ordem das rotas referente ao modulo canteen

--- backend/app/canteen/api/views.py
+++ backend/app/canteen/api/views.py
@@ -27,24 +27,23 @@ def register_daily_menu(request, data: DailyMenuIn):
     response = use_case.execute(dto)
     return 201, DailyMenuOut.from_domain(response)
 
-
 @router.get('/{id}', response={200: DailyMenuOut})
 def view_by_id(request, id: UUID):
-    use_case = canteen_container.return_with_id_use_case()
+    use_case = canteen_container.daily_menu_return_with_id_use_case()
     response = use_case.execute(id)
     return 200, DailyMenuOut.from_domain(response)
 
 
 @router.get('/', response={200: DailyMenuOut})
 def view_daily_menu(request, date: Date):
-    use_case = canteen_container.return_daily_menu_use_case()
+    use_case = canteen_container.daily_menu_return_use_case()
     response = use_case.execute(date)
     return 200, DailyMenuOut.from_domain(response)
 
 
-@router.get('/date_range', response={200: List[DailyMenuOut]})
+@router.get('/date_range/', response={200: List[DailyMenuOut]})
 def view_with_date_range(request, from_date: Date, to_date: Date):
-    use_case = canteen_container.return_with_date_range_use_case()
+    use_case = canteen_container.daily_menu_return_with_date_range_use_case()
     entities = use_case.execute(from_date, to_date)
     return 200, [DailyMenuOut.from_domain(entity) for entity in entities]
 
@@ -52,7 +51,7 @@ def view_with_date_range(request, from_date: Date, to_date: Date):
 @router.patch('/{id}', response={200: DailyMenuOut})
 @atomic
 def update_daily_menu(request, id: UUID, data: DailyMenuUpdate):
-    use_case = canteen_container.update_daily_menu_use_case()
+    use_case = canteen_container.daily_menu_update_use_case()
     dto = data.to_dto()
     response = use_case.execute(id, dto)
     return 200, DailyMenuOut.from_domain(response)
