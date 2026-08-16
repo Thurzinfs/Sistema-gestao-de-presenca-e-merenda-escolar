725cec910d72f331bd606fc83ae24a0f1b508861
Thurzinfs <francasilvaarthur95@gmail.com>
2026-08-16 16:50:36 -0300

    feat: adiciona a requisicao referente a busca de sala e da quantidade registro de almoco

--- frontend/dist/request/api.js
+++ frontend/dist/request/api.js
@@ -1,6 +1,6 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios/+esm';
 
-import { API_BASE_URL } from '../config.js';
+import API_BASE_URL from '../../config.js';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -12,10 +12,30 @@ const api = axios.create({
 
 export const getStudentList = async () => {
   try {
-    const response = await api.get('/academic/students/list/active');
+    const response = await api.get('/api/v1/academic/students/list/all');
     return response.data;
   } catch (error) {
-    console.error('Error fetching student list:', error);
+    console.error('Error: ', error);
     throw error;
   }
 };
+
+export const getClassroom = async (id) => {
+  try {
+    const response = await api.get(`/api/v1/academic/classroom/${id}`);
+    return response.data;
+  } catch (eror) {
+    console.error('Error: ', error);
+    throw error;
+  }
+}
+
+export const getSnackRegisters = async () => {
+  try {
+    const response = await api.get(`/api/v1/presence/registerSnack/`)
+    return response.data;
+  } catch (error) {
+    console.error('Error: ', error);
+    throw error;
+  }
+}
