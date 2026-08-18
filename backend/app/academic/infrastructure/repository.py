5fe24e1cbd2c4ae65ca692d83b67f88cf0fd14f8
silvaphb <jeferson.aps170@gmail.com>
2026-08-18 01:00:59 -0300

    fix: corrige ausencia do metodo first na solicitação do banco de dados

--- backend/app/academic/infrastructure/repository.py
+++ backend/app/academic/infrastructure/repository.py
@@ -93,7 +93,7 @@ class StudentsRepository(IStudentsRepository):
 
     def find_students_by_qrcode(self, qr_code: str) -> StudentsEntity | None:
         try:
-            return self._to_entity(Student.objects.get(qr_code=qr_code))
+            return self._to_entity(Student.objects.filter(qr_code=qr_code).first())
         except Student.DoesNotExist:
             return None
 
